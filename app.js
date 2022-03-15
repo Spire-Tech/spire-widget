26d411cb7cf801832b989185e505be1a05d6f725
Saviour Essien <essiensaviour.a@gmail.com>
2022-03-15 20:34:41 +0100

    Made changes to the CSS file

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ window.addEventListener('load', function(event) {
     const overlay = document.createElement("div");
     overlay.classList.add("widget-overlay");
     const close = document.createElement("div");
-    close.classList.add("close");
+    close.classList.add("spire_close");
     close.textContent = "x";
 
       const formData = new FormData();
@@ -24,13 +24,13 @@ window.addEventListener('load', function(event) {
     const buttonText = document.createElement("span");
     buttonImage.src = "https://res.cloudinary.com/spire-tech/image/upload/v1646986830/icons/icon-feedback_pvxbmu.svg";
     buttonText.textContent = "Leave a Feedback";
-    button.classList.add("feedback-button");
+    button.classList.add("spire_feedback_button");
 
     button.append(buttonImage, buttonText);
     const companyName = document.createElement("h3");
-    companyName.classList.add("company-name");
+    companyName.classList.add("spire_company_name");
     const companyLogoContainer = document.createElement("div");
-    companyLogoContainer.classList.add("spire-logo");
+    companyLogoContainer.classList.add("spire_logo");
     const companyLogo = document.createElement("img");
     companyLogo.src = "https://res.cloudinary.com/spire-tech/image/upload/v1646986832/icons/spire-logo_fwxm5y.svg";
     companyLogoContainer.appendChild(companyLogo);
@@ -38,31 +38,31 @@ window.addEventListener('load', function(event) {
 
 
     const widgetContainer = document.createElement("div");
-    widgetContainer.classList.add("widget");
+    widgetContainer.classList.add("spire_widget");
     const back = document.createElement("div");
-    back.classList.add("back");
+    back.classList.add("spire_back");
     const backIcon = document.createElement("img");
     backIcon.src = "https://res.cloudinary.com/spire-tech/image/upload/v1646986830/icons/arrow_uhkexa.svg";
     back.appendChild(backIcon);
     const feedblockTitle = document.createElement("h4");
-    feedblockTitle.classList.add("rating-title");
+    feedblockTitle.classList.add("spire_rating_title");
     const feedblockSubTitle = document.createElement("p");
-    feedblockSubTitle.classList.add("rating-subTitle");
+    feedblockSubTitle.classList.add("spire_rating_subTitle");
 
 
     const ratingBlock = document.createElement("div");
-    ratingBlock.classList.add("rating-block");
+    ratingBlock.classList.add("spire_rating_block");
     const ratingStars = document.createElement("div");
-    ratingStars.classList.add("rating-stars");
+    ratingStars.classList.add("spire_rating_stars");
     const ratingStarChecked = document.createElement("div");
-    ratingStarChecked.classList.add("rating-stars");
+    ratingStarChecked.classList.add("spire_rating_stars");
     ratingStarChecked.style.display = "none";
 
 
     for (let index = 0; index < 5; index++) {
         const star = document.createElement("img");
         star.src = "https://res.cloudinary.com/spire-tech/image/upload/v1646986833/icons/star_mj9lej.svg";
-        star.classList.add("stars");
+        star.classList.add("spire_stars");
         
         ratingStars.append(star);
 
@@ -129,7 +129,7 @@ window.addEventListener('load', function(event) {
     // ratingContainer.appendChild(emptyStarImage);
 
     const ratingContainer = document.createElement("div");
-    ratingContainer.classList.add("form-stars");
+    ratingContainer.classList.add("spire_form_stars");
     const emptyRatingStar = document.createElement("img");
     emptyRatingStar.src = "https://res.cloudinary.com/spire-tech/image/upload/v1646986833/icons/star_mj9lej.svg";
     
@@ -141,15 +141,15 @@ window.addEventListener('load', function(event) {
 
 
     const form = document.createElement("form");
-    form.classList.add("form");
+    form.classList.add("spire_form");
     
     const emailContainer = document.createElement("div");
-    emailContainer.classList.add("form-input");
+    emailContainer.classList.add("spire_form_input");
     const emailField = document.createElement("input");
     emailField.type = "email";
     emailField.name = "email";
     emailField.placeholder = "example@gmail.com";
-    emailField.classList.add("form-input-input");
+    emailField.classList.add("spire_form_input_input");
     emailField.setAttribute("id", "email");
     emailField.setAttribute("required", true);
 
@@ -158,7 +158,7 @@ window.addEventListener('load', function(event) {
     emailLabelSpan.textContent = "What’s your email?";
     const emailLabelSmall = document.createElement("small");
     emailLabel.setAttribute("for", "email");
-    emailLabel.classList.add("form-input-label");
+    emailLabel.classList.add("spire_form_input_label");
     emailLabelSmall.textContent = "* Required";
     emailLabel.append(emailLabelSpan, emailLabelSmall);
 
@@ -166,14 +166,14 @@ window.addEventListener('load', function(event) {
 
 
     const commentContainer = document.createElement("div");
-    commentContainer.classList.add("form-textarea");
+    commentContainer.classList.add("spire_form_textarea");
     const commentLabel = document.createElement("label");
     const commentLabelSpan = document.createElement("span");
     commentLabel.setAttribute("for", "comment");
     commentLabelSpan.textContent = "What’s the issue?";
     const commentLabelSmall = document.createElement("small");
     commentLabel.setAttribute("for", "comment");
-    commentLabel.classList.add("form-textarea-label");
+    commentLabel.classList.add("spire_form_textarea_label");
     commentLabelSmall.textContent = "* Required";
     commentLabel.append(commentLabelSpan, commentLabelSmall);
 
@@ -182,9 +182,9 @@ window.addEventListener('load', function(event) {
     commentField.setAttribute("required", true);
     commentField.setAttribute("id", "comment");
     commentField.setAttribute("cols", "20");
-    commentField.setAttribute("rows", "10");
+    commentField.setAttribute("rows", "4");
     commentField.name = "comment";
-    commentField.classList.add("form-textarea-textarea")
+    commentField.classList.add("spire_form_textarea_textarea")
 
     commentContainer.append(commentLabel, commentField);
 
@@ -192,14 +192,14 @@ window.addEventListener('load', function(event) {
 
 
     const screenshotContainer = document.createElement("label");
-    screenshotContainer.classList.add("image-input");
+    screenshotContainer.classList.add("spire_image_input");
     const screenshotInput = document.createElement("input");
     screenshotInput.classList.add("custom-file-input");
     screenshotInput.type = "file";
     screenshotInput.multiple = true;
     screenshotInput.name = "screenshot";
     screenshotInput.placeholder = "Upload an Image";
-    screenshotInput.classList.add("custom-file-input");
+    screenshotInput.classList.add("spire_custom_file_input");
     screenshotInput.setAttribute("id", "screenshot");
     const screenshotIcon = document.createElement("img");
     screenshotIcon.src = "https://res.cloudinary.com/spire-tech/image/upload/v1646986831/icons/iconCamera_tweagv.svg";
@@ -210,40 +210,40 @@ window.addEventListener('load', function(event) {
 
     //submit-button
     const buttonContainer = document.createElement("div");
-    buttonContainer.classList.add("form-button");
+    buttonContainer.classList.add("spire_form_button");
     const submitButton = document.createElement("button");
     submitButton.setAttribute("type", "submit");
     submitButton.textContent = "Submit";
-    submitButton.classList.add("form-button-button")
+    submitButton.classList.add("spire_form_button_button")
     buttonContainer.appendChild(submitButton);
 
 
     const container = document.createElement("div");
-    container.classList.add("container", "disable");
+    container.classList.add("spire_container", "disable");
     const feedblocks = document.createElement("div");
-    feedblocks.classList.add("feedblocks");
+    feedblocks.classList.add("spire_feedblocks");
     
     
     
 
     button.addEventListener("click", function(event) {
         const overlay = document.querySelector(".widget-overlay");
-        const container = document.querySelector(".container");
-        const close = document.querySelector(".close");
-        if (overlay.classList.contains('active') && container.classList.contains('active')) {
-            overlay.classList.remove("active");
-            overlay.classList.add("disable");
-            container.classList.remove("active");
-            container.classList.add("disable");
-            close.classList.remove("active");
-            close.classList.add("disable");
+        const container = document.querySelector(".spire_container");
+        const close = document.querySelector(".spire_close");
+        if (overlay.classList.contains('spire_active') && container.classList.contains('spire_active')) {
+            overlay.classList.remove("spire_active");
+            overlay.classList.add("spire_disable");
+            container.classList.remove("spire_active");
+            container.classList.add("spire_disable");
+            close.classList.remove("spire_active");
+            close.classList.add("spire_disable");
           } else {
-            overlay.classList.remove("disable");
-            overlay.classList.add("active");
-            container.classList.remove("disable");
-            container.classList.add("active");
-            close.classList.remove("disable");
-            close.classList.add("close", "active");
+            overlay.classList.remove("spire_disable");
+            overlay.classList.add("spire_active");
+            container.classList.remove("spire_disable");
+            container.classList.add("spire_active");
+            close.classList.remove("spire_disable");
+            close.classList.add("spire_close", "spire_active");
             
           }
     });
@@ -251,22 +251,22 @@ window.addEventListener('load', function(event) {
 
     close.addEventListener("click", function(event) {
       const overlay = document.querySelector(".widget-overlay");
-      const container = document.querySelector(".container");
-      const close = document.querySelector(".close");
-      if (overlay.classList.contains('active') && container.classList.contains('active') && close.classList.contains('active')) {
-          overlay.classList.remove("active");
-          overlay.classList.add("disable");
-          container.classList.remove("active");
-          container.classList.add("disable");
-          close.classList.remove("active");
-          close.classList.add("disable");
+      const container = document.querySelector(".spire_container");
+      const close = document.querySelector(".spire_close");
+      if (overlay.classList.contains('spire_active') && container.classList.contains('spire_active') && close.classList.contains('spire_active')) {
+          overlay.classList.remove("spire_active");
+          overlay.classList.add("spire_disable");
+          container.classList.remove("spire_active");
+          container.classList.add("spire_disable");
+          close.classList.remove("spire_active");
+          close.classList.add("spire_disable");
         } else {
-          overlay.classList.remove("active");
-          overlay.classList.add("disable");
-          container.classList.remove("active");
-          container.classList.add("disable");
-          close.classList.remove("active");
-          close.classList.add("disable");
+          overlay.classList.remove("spire_active");
+          overlay.classList.add("spire_disable");
+          container.classList.remove("spire_active");
+          container.classList.add("spire_disable");
+          close.classList.remove("spire_active");
+          close.classList.add("spire_disable");
         }
   });
     
@@ -323,15 +323,15 @@ window.addEventListener('load', function(event) {
               }
               console.log(block);
             feedblocks.innerHTML += `
-                <div class="feedblock">
-                    <div class="feedblock-img">
+                <div class="spire_feedblock">
+                    <div class="spire_feedblock_img">
                         ${image}
                     </div>
                     <div>
-                        <h4 class="feedblock-title">${block.title}</h4>
-                        <p class="feedblock-subTitle">${block.subTitle}</p>
-                        <span class="slug" style={display: "none"}>${block.slug}</span>
-                        <span class="block" style={display: "none"}>${block.id}</span>
+                        <h4 class="spire_feedblock_title">${block.title}</h4>
+                        <p class="spire_feedblock_subTitle">${block.subTitle}</p>
+                        <span class="spire_slug" style={display: "none"}>${block.slug}</span>
+                        <span class="spire_block" style={display: "none"}>${block.id}</span>
                     </div>
                 </div>`;
           });
@@ -340,7 +340,7 @@ window.addEventListener('load', function(event) {
 
 
         const imagePreview = document.createElement("div");
-        imagePreview.classList.add("image-preview");
+        imagePreview.classList.add("spire_image_preview");
         
 
       
@@ -367,25 +367,25 @@ window.addEventListener('load', function(event) {
           
           feedblocks.addEventListener("click", function(event) {
             console.log(event);
-            feedblockId = event.path[2].querySelector(".block").textContent;
+            feedblockId = event.path[2].querySelector(".spire_block").textContent;
             console.log(feedblockId);
-            console.log(event.path[2].querySelector(".block").textContent);
-            if (event.path[2].querySelector(".slug").textContent === "ratings") {
+            console.log(event.path[2].querySelector(".spire_block").textContent);
+            if (event.path[2].querySelector(".spire_slug").textContent === "ratings") {
                 ratingBlock.style.display = "flex";
                 feedblocks.style.display = "none";
                 back.style.display = "flex";
                 feedblockTitle.style.display = "block";
                 feedblockSubTitle.style.display = "block";
-                feedblockTitle.textContent = event.path[2].querySelector(".feedblock-title").textContent;
-                feedblockSubTitle.textContent = event.path[2].querySelector(".feedblock-subTitle").textContent;
+                feedblockTitle.textContent = event.path[2].querySelector(".spire_feedblock_title").textContent;
+                feedblockSubTitle.textContent = event.path[2].querySelector(".spire_feedblock_subTitle").textContent;
                 screenshotContainer.style.display = "none";
                 ratingContainer.style.display = "flex";
                 
-            } else if (event.path[2].querySelector(".slug").textContent === "issue-reports") {
+            } else if (event.path[2].querySelector(".spire_slug").textContent === "issue-reports") {
                 feedblockTitle.style.display = "block";
                 feedblockSubTitle.style.display = "block";
-                feedblockTitle.textContent = event.path[2].querySelector(".feedblock-title").textContent;
-                feedblockSubTitle.textContent = event.path[2].querySelector(".feedblock-subTitle").textContent
+                feedblockTitle.textContent = event.path[2].querySelector(".spire_feedblock_title").textContent;
+                feedblockSubTitle.textContent = event.path[2].querySelector(".spire_feedblock_subTitle").textContent
                 form.style.display = "block";
                 back.style.display = "flex";
                 feedblocks.style.display = "none";
@@ -394,8 +394,8 @@ window.addEventListener('load', function(event) {
             } else {
                 feedblockTitle.style.display = "block";
                 feedblockSubTitle.style.display = "block";
-                feedblockTitle.textContent = event.path[2].querySelector(".feedblock-title").textContent;
-                feedblockSubTitle.textContent = event.path[2].querySelector(".feedblock-subTitle").textContent
+                feedblockTitle.textContent = event.path[2].querySelector(".spire_feedblock_title").textContent;
+                feedblockSubTitle.textContent = event.path[2].querySelector(".spire_feedblock_subTitle").textContent
                 form.style.display = "block";
                 back.style.display = "flex";
                 feedblocks.style.display = "none";
@@ -405,18 +405,18 @@ window.addEventListener('load', function(event) {
           });
 
           const submissionContainer = document.createElement("div");
-          submissionContainer.classList.add("submission");
+          submissionContainer.classList.add("spire_submission");
           const submissionFrame = document.createElement("iframe");
-          submissionFrame.classList.add("submission-frame");
+          submissionFrame.classList.add("spire_submission_frame");
           submissionFrame.src = "https://embed.lottiefiles.com/animation/86372";
           const submissionTitle = document.createElement("h3");
-          submissionTitle.classList.add("submission-title");
+          submissionTitle.classList.add("spire_submission_title");
           submissionTitle.textContent = "Submission received!";
           const submissionSubTitle = document.createElement("p");
-          submissionSubTitle.classList.add("submission-subTitle");
+          submissionSubTitle.classList.add("spire_submission-_ubTitle");
           submissionSubTitle.textContent = "Thank you for taking the time to let us know about your experience.";
           const submissionButton = document.createElement("button");
-          submissionButton.classList.add("submission-button");
+          submissionButton.classList.add("spire_submission_button");
           submissionButton.setAttribute("type", "button");
           submissionButton.textContent = "Write Another Feedback";
           submissionButton.addEventListener("click", function(event) {
