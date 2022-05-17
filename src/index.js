db15c595139b7c79f92ee5321c9bebb85af49ca4
Barnabas Asha <barnabee58@gmail.com>
2022-05-17 20:10:30 +0100

    created templates for all views, tried fixing font issue

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
+import styles from './index.css'
 import { createTemplate } from "./template"
 
 const TAG_NAME = 'spire-feedjet'
 
-export default function SpireWidget () {
-  const template = createTemplate("heyyyy")
+export default function SpireWidget() {
+  const template = createTemplate()
 
   class SpireFeedjetElement extends HTMLElement {
     constructor() {
       super();
       const shadowDOM = this.attachShadow({ mode: 'open' });
+
+      const linkElem = document.createElement('link');
+    linkElem.setAttribute('rel', 'stylesheet');
+    linkElem.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap');
+
       // Render the template in the shadow dom
+      // Create some CSS to apply to the shadow DOM
+      const style = document.createElement('style');
+      style.textContent = styles
+
+        // attach the created elements to the shadow DOM
+        shadowDOM.appendChild(linkElem)
+      shadowDOM.appendChild(style)
       shadowDOM.appendChild(template.content.cloneNode(true));
     }
   }
@@ -17,13 +30,13 @@ export default function SpireWidget () {
     customElements.define(TAG_NAME, SpireFeedjetElement);
   }
 
-   // create an instance of the component
-   const componentInstance = document.createElement(TAG_NAME, {
+  // create an instance of the component
+  const componentInstance = document.createElement(TAG_NAME, {
     is: TAG_NAME,
   });
   // mount the component instance in the body element
   const container = document.body;
-  container.appendChild(componentInstance);
+  container.append(componentInstance);
   // returning the instance will be useful later
   return componentInstance;
 }
