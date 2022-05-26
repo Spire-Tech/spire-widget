de9d5dcdbbd65c5ea74587ea5d5ea765926affb6
Barnabas Asha <barnabee58@gmail.com>
2022-05-26 14:26:41 +0100

    fixes to ratings component

--- src/components/views/ratingsComponent.js
+++ src/components/views/ratingsComponent.js
@@ -14,13 +14,15 @@ const RatingsComponent = ({ setRatings }) => {
   const handleVisual = (state) => {
     switch (state) {
       case 1:
-        return (value == 1) || (value == 2) || (value == 3) || (value == 4)
+        return (value == 1) || (value == 2) || (value == 3) || (value == 4) || (value == 5)
       case 2:
-        return (value == 2) || (value == 3) || (value == 4)
+        return (value == 2) || (value == 3) || (value == 4) || (value == 5)
       case 3:
-        return (value == 3) || (value == 4)
+        return (value == 3) || (value == 4) || (value == 5)
       case 4:
-        return value == 4
+        return (value == 4) || (value == 5)
+      case 5:
+        return value == 5
       default:
         throw new Error('Invalid state')
     }
@@ -43,6 +45,10 @@ const RatingsComponent = ({ setRatings }) => {
       <label class={`${styles.__ratings_btn} ${handleVisual(4) && styles.__active}`}>
         <StarIcon />
         <input class={styles.__ratings_input} type="radio" name="ratings" value={4} />
+      </label>
+      <label class={`${styles.__ratings_btn} ${handleVisual(5) && styles.__active}`}>
+        <StarIcon />
+        <input class={styles.__ratings_input} type="radio" name="ratings" value={5} />
       </label>
     </div>
   )
